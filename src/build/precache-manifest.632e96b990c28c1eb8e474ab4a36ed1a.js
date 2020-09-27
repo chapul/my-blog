c765dc7edd534a5f89e29c6016dd00ac8f74f5a7
Sebastian <e.sebastian1978@gmail.com>
2020-09-27 21:49:24 +0100

    First commit

---
+++ src/build/precache-manifest.632e96b990c28c1eb8e474ab4a36ed1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d681b6b43523af99138ca0d1b47985",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92ef15fb8f8e6107b5d6",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "8dde1b205a34dcd30da5",
+    "url": "/static/js/2.33f3023b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.33f3023b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ef15fb8f8e6107b5d6",
+    "url": "/static/js/main.c9a94503.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
